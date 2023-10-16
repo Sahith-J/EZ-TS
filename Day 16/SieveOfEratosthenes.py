5bd24528e0dcb4dc987de9cf56138851b72f34f3
Sahith-J <135148023+Sahith-J@users.noreply.github.com>
2023-10-16 08:41:00 +0530

    Add files via upload

---
+++ Day 16/SieveOfEratosthenes.py
@@ -0,0 +1,21 @@
+def SieveOfEratosthenes(n): 
+    prime = [True for i in range(n+1)] 
+    p = 2
+    while (p * p <= n): 
+        if (prime[p] == True): 
+            for i in range(p * p, n+1, p): 
+                prime[i] = False
+        p += 1
+    for p in range(2, n+1): 
+        if prime[p]: 
+            print(p,end=" ") 
+
+n=int(input())
+SieveOfEratosthenes(n)
+
+
+
+# O (nk) is the time complexity where k is the no of primes in range n 
+
+
+
